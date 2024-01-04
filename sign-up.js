d893bb13c5b6059070635a179ac1dad1e1065269
Nikil <nik300601@gmail.com>
2024-01-04 14:37:07 +0000

    updated form code

--- sign-up.js
+++ sign-up.js
@@ -3,12 +3,14 @@ let confirmPass = document.querySelector('.confirm-pass')
 
 let password = document.querySelectorAll('#password');
 password.forEach(element => {
-    element.addEventListener('keyup', () => {
-        if (confirmPass.value == pass.value) {
-            element.style.border = '2px solid rgb(173, 173, 173)';
+    element.addEventListener('focusout', () => {
+        if (confirmPass.value === pass.value) {
+            pass.style.border = '2px solid rgb(173, 173, 173)';
+            confirmPass.style.border = '2px solid rgb(173, 173, 173)';
         }
         else {
-            element.style.border = '2px solid red';
+            pass.style.border = '2px solid red';
+            confirmPass.style.border = '2px solid red';
 
         }
     } )
